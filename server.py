913c9bffb2a50b7f85dbf121af914baaf82e06a3
Cooldownn <dphung291@gmail.com>
2019-07-25 23:33:18 +0700

    fix

--- server.py
+++ server.py
@@ -26,7 +26,7 @@ def result():
                                 index=['input'])
     model = pickle.load(open("model_satisfaction.pkl","rb"))
     prediction = model.predict(input_variables)
-    return str(prediction)
+    return jsonify(int(prediction))
 #    ret = "prediction: " + str(prediction)
 #    if (prediction == 1):
 #        ret = "Yes"
