ae0e8ea13aabf54775b059483b6c8b036408e3fe
Cooldownn <dphung291@gmail.com>
2019-07-25 17:14:40 +0700

    add jsonify

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import flask
 import pickle
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 #creating instance of the class
 app=Flask(__name__)
@@ -25,11 +25,11 @@ def result():
     model = pickle.load(open("model_satisfaction.pkl","rb"))
     prediction = model.predict(input_variables)
 #    ret = "prediction: " + str(prediction)
-    if (prediction == 1):
-        ret = "Yes"
-    elif (prediction == 0) :
-        ret = "No"
-    return ret
+#    if (prediction == 1):
+#        ret = "Yes"
+#    elif (prediction == 0) :
+#        ret = "No"
+    return jsonify(prediction)
 # {"data": "2, 2, 3, 4, 3, 4, 3, 5, 4, 5001, 0 "}
 
 @app.route('/promotion', methods = ['POST'])
@@ -45,10 +45,10 @@ def ret():
     model = pickle.load(open("model_promotion.pkl","rb"))
     prediction = model.predict(input_variables)
 #    ret = "prediction: " + str(prediction)
-    if (prediction == 1):
-        ret = "Yes"
-    elif (prediction == 0) :
-        ret = "No"
-    return ret
+#    if (prediction == 1):
+#        ret = "Yes"
+#    elif (prediction == 0) :
+#        ret = "No"
+    return jsonify(prediction)
 
 # {"data": "4, 3, 5561, 0, 16, 3, 6, 5"}
